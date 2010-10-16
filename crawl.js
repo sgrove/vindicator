350916cc436289da55fbd66c6a18f37180afc7e3
Sean Grove <sean@fakecoolguys.com>
2010-10-16 13:04:21 -0700

    Try to make it better by breaking everything.

--- crawl.js
+++ crawl.js
@@ -1,8 +1,8 @@
 var sys = require('sys')
-    ,http = require('http')
-    ,request = require('request')
-    ,jsdom = require('jsdom')
-    ,window = jsdom.jsdom().createWindow();
+,http = require('http')
+,request = require('request')
+,jsdom = require('jsdom')
+,window = jsdom.jsdom().createWindow();
 
 
 Array.prototype.include = function(needle) {
@@ -22,38 +22,73 @@ String.prototype.startsWith = function(str) {
 }
 
 var results = []
-   ,visited = [];
+,visited = []
+,pending = [];
 
-var crawl = function(url) {
-    request({uri:url}, function (error, response, body) {
-        results.push([response.statusCode, url]);
 
-        console.log("[" + response.statusCode + "] " + url);
-
-        if (!error && response.statusCode == 200) {
-            var window = jsdom.jsdom(body).createWindow();
-            jsdom.jQueryify(window, "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js", function (window, $) {
-                $('a').each(function() {
-                    var newUrl = $(this).attr("href");
-
-                    if (newUrl.startsWith("//")) {
-                        newUrl = newUrl.substring(1, newUrl.length);
-                    }
-                        
-                    if (!newUrl.startsWith("http")) {
-                        var protocol = response.socket.https ? "https://" : "http://";
-                        newUrl = protocol + response.socket.host + newUrl;
-                    }
-
-                    if (!visited.include(newUrl)) {
-                        visited.push(newUrl);
-                        crawl(newUrl);
-                    }
-                });
-            });
+var operator = {
+    results: [],
+    visited: [],
+    pending: [],
+    currentConnections: 0,
+    maxConnections: 20,
+    addUrl: function(url) { if (!this.visited.include(url)) { console.log("Adding: " + url); this.pending.push(url); return true; } else { return false; } },
+    nextUrl: function() {
+        if (this.pending.length != 0) {
+            this.currentConnections += 1;
+            return this.pending.shift();
+        } else {
+            return false;
         }
-    });
-}
+    },
+    hasPendingUrls: function () { return !(this.pending.length == 0); },
+    logResult: function(url, status) { this.currentConnections = Math.max(0, this.currentConnections - 1); this.results.push([url, status]); this.visited.push(url); },
+    crawl: function(url, cb) {
+        //console.log("crawling: " + url);
+        var op = this;
+        request({uri:url}, function (error, response, body) {
+            op.logResult(url, response.statusCode);
+
+            if (!error && response.statusCode == 200) {
+                var window = jsdom.jsdom(body).createWindow();
+                jsdom.jQueryify(window, "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js", function (window, $) {
+                    $('a').each(function() {
+                        var newUrl = $(this).attr("href");
+
+                        if (newUrl.startsWith("//")) {
+                            newUrl = newUrl.substring(1, newUrl.length);
+                        }
+                        
+                        if (!newUrl.startsWith("http")) {
+                            var protocol = response.socket.https ? "https://" : "http://";
+                            newUrl = protocol + response.socket.host + "/" + newUrl;
+                        }
+
+                        op.addUrl(newUrl);
+                        cb();
+                    });
+                });
+            }
+        });
+    },
+    crawlNext: function(cb) { if ((this.currentConnections < this.maxConnections)) {
+        //console.log("Crawling next...");
+        var next = this.nextUrl()
+        if (next) {
+            this.crawl(next, cb);
+        }
+    }
+                            }
+};
 
 var startingUrl = process.argv[2];
-crawl(startingUrl);
+operator.addUrl( startingUrl);
+
+while(true) {
+    //console.log(operator);
+    operator.crawlNext(function() {
+        console.log("/------------------------------------------------------------\\");
+        console.log(operator.results);
+        console.log("\\------------------------------------------------------------/");
+    });
+}
